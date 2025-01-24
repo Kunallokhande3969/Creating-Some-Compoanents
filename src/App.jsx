7a0ae753caba3adbbd24e56308af04690560e034
kunal lokhande <kunallokhande3969@gmail.com>
2025-01-24 23:44:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from './components/Nav.jsx';
+import Footer from './components/Footer.jsx';
+import Mid from './components/Mid.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Footer/>
+      <Mid/>
+    </div>
+  )
+}
+
+export default App
